40c02679cd4f1becdcef36d6b84592efdbe82506
Yash Pratap Singh Solanki <yashpratap0143@gmail.com>
2025-11-21 13:11:31 +0530

    Deleted frontend+backend

--- frontend/components/CustomCursor.jsx
+++
@@ -1,8 +0,0 @@
-import { useCustomCursor } from '../hooks/useCustomCursor'
-
-export default function CustomCursor() {
-  const cursorRef = useCustomCursor()
-
-  return <div className="custom-cursor" ref={cursorRef} />
-}
-
