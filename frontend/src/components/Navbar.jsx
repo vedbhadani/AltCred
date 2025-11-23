4b0165a237b21a911c756aa0f947e15ed5aa84ae
Maxiemad <akanksha.k@adypu.edu.in>
2025-11-23 12:08:08 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,7 @@
-
 import Logo from "./Logo"
-const Navbar = ()=> {
-    return <>
+const Navbar=()=>{
+    return <div style={{display:'flex',alignItems:'center',gap:'20px'}}>
         <Logo />
-    </>
-}
+        <div style={{height:'2px',flex:1,background:'linear-gradient(to right, #000000, #40a8c8)'}}></div>
+    </div>}
 export default Navbar;
