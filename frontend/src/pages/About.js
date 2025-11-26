fb0fbc1433c08b0a9570d2eeadc13065727e479e
Maxiemad <akanksha.k@adypu.edu.in>
2025-11-26 11:54:43 +0530

    Fix: Add default React component exports to Contact and other page files

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Welcome to AltCred. Learn more about us here.</p>
+    </div>
+  );
+}
