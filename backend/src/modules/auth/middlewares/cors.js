34211cbcc63980b92c5f393cc3b68f4660ed062b
vedbhadani <vedbhadani157@gmail.com>
2025-12-01 13:46:38 +0530

    changes for deployment

--- backend/src/modules/auth/middlewares/cors.js
+++ backend/src/modules/auth/middlewares/cors.js
@@ -1,5 +1,5 @@
 const cors = require("cors")
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:4000", "http://localhost:5000", "http://localhost:3001"]
+const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:4000", "http://localhost:5000", "http://localhost:3001","https://altcred-1adk.vercel.app"]
 
 const corsOptions = cors({
   origin: function (origin, callback) {
