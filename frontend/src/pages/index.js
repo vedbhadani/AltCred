f1ba2d28793ce9fece704596ab781fe16b83aa29
Yash Pratap Singh Solanki <yashpratap0143@gmail.com>
2025-12-01 09:44:57 +0530

    Created coming-soon page and updated all empty footer links

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -148,7 +148,7 @@ export default function Home() {
                     </div>
                 </section>
 
-                <section className="py-24 px-4 sm:px-6 lg:px-8">
+                <section id="features" className="py-24 px-4 sm:px-6 lg:px-8">
                     <div className="max-w-7xl mx-auto">
                         <motion.div
                             initial={{ opacity: 0, y: 50 }}
@@ -182,7 +182,7 @@ export default function Home() {
                     </div>
                 </section>
 
-                <section className="py-24 px-4 sm:px-6 lg:px-8">
+                <section id="lenders" className="py-24 px-4 sm:px-6 lg:px-8">
                     <div className="max-w-7xl mx-auto">
                         <motion.div
                             initial={{ opacity: 0, y: 50 }}
